3e32f8490d24aa228beec90fc95fff1e4fba0d35
Astrobot268 <72189934+Astrobot268@users.noreply.github.com>
2021-03-11 10:20:33 +0530

    v8commit

--- main.py
+++ main.py
@@ -11,13 +11,14 @@ magic_ball = ['As I see it, yes.','Ask again later','Better not tell you now','C
 async def on_ready():
     print(" Bot is ready")
 
-x =  'On '+str(len(client.guilds)) + ' Servers'
+
 #print(x)
 
 #discod presense
 async def ch_pr():
     await client.wait_until_ready()
-    statues = ['Discord.py',x,'help||prefix-#','Under DevelopMent','sleeping']
+    x =  'On '+str(len(client.guilds)) + ' Servers'
+    statues = ['Discord.py',x,'help || prefix-#','Under Development','Sleeping','Version - 0.7']
     
     while not client.is_closed():
         status = random.choice(statues)
@@ -89,6 +90,14 @@ async def h(ctx):
 
 
 
+
+
+
+
+
+
+
+
 @client.command(aliases=['magicball'])
 async def mg(ctx):
     embed = discord.Embed(title='Magic Ball',description='you will get 10 seconds to Ask in your Mind',color=discord.Color.dark_purple())
@@ -103,6 +112,26 @@ async def mg(ctx):
 
 
 
+
+@client.command(aliases=['usif'])
+async def userinfo(ctx, *, user: discord.User = None): # b'\xfc'
+    if user is None:
+        user = ctx.author      
+    date_format = "%a, %d %b %Y %I:%M %p"
+    embed = discord.Embed(color=0xdfa3ff, description=user.mention)
+    embed.set_author(name=str(user), icon_url=user.avatar_url)
+    embed.set_thumbnail(url=user.avatar_url)
+    embed.add_field(name="Joined", value=user.joined_at.strftime(date_format))
+    members = sorted(ctx.guild.members, key=lambda m: m.joined_at)
+    embed.add_field(name="Join position", value=str(members.index(user)+1))
+    embed.add_field(name="Registered", value=user.created_at.strftime(date_format))
+    if len(user.roles) > 1:
+        role_string = ' '.join([r.mention for r in user.roles][1:])
+        embed.add_field(name="Roles [{}]".format(len(user.roles)-1), value=role_string, inline=False)
+    perm_string = ', '.join([str(p[0]).replace("_", " ").title() for p in user.guild_permissions if p[1]])
+    embed.add_field(name="Guild permissions", value=perm_string, inline=False)
+    embed.set_footer(text='ID: ' + str(user.id))
+    return await ctx.send(embed=embed)
 
 
 
