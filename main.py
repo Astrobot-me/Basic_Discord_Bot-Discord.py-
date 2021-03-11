aecc07b02dc35fcffbeda7f7f691db4b46aab7dc
Astrobot268 <72189934+Astrobot268@users.noreply.github.com>
2021-03-11 10:31:29 +0530

    new

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import discord
 from discord.ext import commands
 import random
 import asyncio
+from cogs.utils.checks import embed_perms, cmd_prefix_len
+
+
+
+
+
+
 
 client = commands.Bot(command_prefix="#")
 coin = ['HEADs','TAILS']
@@ -113,27 +120,97 @@ async def mg(ctx):
 
 
 
-@client.command(aliases=['usif'])
-async def userinfo(ctx, *, user: discord.User = None): # b'\xfc'
-    if user is None:
-        user = ctx.author      
-    date_format = "%a, %d ,%b "#,%Y, %I:%M %p"
-    embed = discord.Embed(color=0xdfa3ff, description=user.mention)
-    embed.set_author(name=str(user), icon_url=user.avatar_url)
-    embed.set_thumbnail(url=user.avatar_url)
-    embed.add_field(name="Joined", value=user.joined_at.strftime(date_format))
-    members = sorted(ctx.guild.members, key=lambda m: m.joined_at)
-    embed.add_field(name="Join position", value=str(members.index(user)+1))
-    embed.add_field(name="Registered", value=user.created_at.strftime(date_format))
-    if len(user.roles) > 1:
-        role_string = ' '.join([r.mention for r in user.roles][1:])
-        embed.add_field(name="Roles [{}]".format(len(user.roles)-1), value=role_string, inline=False)
-    perm_string = ', '.join([str(p[0]).replace("_", " ").title() for p in user.guild_permissions if p[1]])
-    embed.add_field(name="Guild permissions", value=perm_string, inline=False)
-    embed.set_footer(text='ID: ' + str(user.id))
-    return await ctx.send(embed=embed)
+class Userinfo:
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.group(invoke_without_command=True, aliases=['user', 'uinfo', 'info', 'ui'])
+    async def userinfo(self, ctx, *, name=""):
+        """Get user info. Ex: [p]info @user"""
+        if ctx.invoked_subcommand is None:
+            pre = cmd_prefix_len()
+            if name:
+                try:
+                    user = ctx.message.mentions[0]
+                except IndexError:
+                    user = ctx.guild.get_member_named(name)
+                if not user:
+                    user = ctx.guild.get_member(int(name))
+                if not user:
+                    user = self.bot.get_user(int(name))
+                if not user:
+                    await ctx.send(self.bot.bot_prefix + 'Could not find user.')
+                    return
+            else:
+                user = ctx.message.author
+
+            if user.avatar_url_as(static_format='png')[54:].startswith('a_'):
+                avi = user.avatar_url.rsplit("?", 1)[0]
+            else:
+                avi = user.avatar_url_as(static_format='png')
+            if isinstance(user, discord.Member):
+                role = user.top_role.name
+                if role == "@everyone":
+                    role = "N/A"
+                voice_state = None if not user.voice else user.voice.channel
+            if embed_perms(ctx.message):
+                em = discord.Embed(timestamp=ctx.message.created_at, colour=0x708DD0)
+                em.add_field(name='User ID', value=user.id, inline=True)
+                if isinstance(user, discord.Member):
+                    em.add_field(name='Nick', value=user.nick, inline=True)
+                    em.add_field(name='Status', value=user.status, inline=True)
+                    em.add_field(name='In Voice', value=voice_state, inline=True)
+                    em.add_field(name='Game', value=user.activity, inline=True)
+                    em.add_field(name='Highest Role', value=role, inline=True)
+                em.add_field(name='Account Created', value=user.created_at.__format__('%A, %d. %B %Y @ %H:%M:%S'))
+                if isinstance(user, discord.Member):
+                    em.add_field(name='Join Date', value=user.joined_at.__format__('%A, %d. %B %Y @ %H:%M:%S'))
+                em.set_thumbnail(url=avi)
+                em.set_author(name=user, icon_url='https://i.imgur.com/RHagTDg.png')
+                await ctx.send(embed=em)
+            else:
+                if isinstance(user, discord.Member):
+                    msg = '**User Info:** ```User ID: %s\nNick: %s\nStatus: %s\nIn Voice: %s\nGame: %s\nHighest Role: %s\nAccount Created: %s\nJoin Date: %s\nAvatar url:%s```' % (user.id, user.nick, user.status, voice_state, user.activity, role, user.created_at.__format__('%A, %d. %B %Y @ %H:%M:%S'), user.joined_at.__format__('%A, %d. %B %Y @ %H:%M:%S'), avi)
+                else:
+                    msg = '**User Info:** ```User ID: %s\nAccount Created: %s\nAvatar url:%s```' % (user.id, user.created_at.__format__('%A, %d. %B %Y @ %H:%M:%S'), avi)
+                await ctx.send(self.bot.bot_prefix + msg)
+
+            await ctx.message.delete()
+
+    @userinfo.command()
+    async def avi(self, ctx, txt: str = None):
+        """View bigger version of user's avatar. Ex: [p]info avi @user"""
+        if txt:
+            try:
+                user = ctx.message.mentions[0]
+            except IndexError:
+                user = ctx.guild.get_member_named(txt)
+            if not user:
+                user = ctx.guild.get_member(int(txt))
+            if not user:
+                user = self.bot.get_user(int(txt))
+            if not user:
+                await ctx.send(self.bot.bot_prefix + 'Could not find user.')
+                return
+        else:
+            user = ctx.message.author
+
+        if user.avatar_url_as(static_format='png')[54:].startswith('a_'):
+            avi = user.avatar_url.rsplit("?", 1)[0]
+        else:
+            avi = user.avatar_url_as(static_format='png')
+        if embed_perms(ctx.message):
+            em = discord.Embed(colour=0x708DD0)
+            em.set_image(url=avi)
+            await ctx.send(embed=em)
+        else:
+            await ctx.send(self.bot.bot_prefix + avi)
+        await ctx.message.delete()
 
 
+def setup(bot):
+    bot.add_cog(Userinfo(bot))
 
 
 
