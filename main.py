187ff3ed2f671ce2ef3f8161660aea0caff5da5d
Astrobot268 <72189934+Astrobot268@users.noreply.github.com>
2021-03-12 18:12:58 +0530

    newcommit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 import random
 import asyncio
-from cogs.utils.checks import embed_perms, cmd_prefix_len
+
 
 
 
@@ -13,7 +13,7 @@ from cogs.utils.checks import embed_perms, cmd_prefix_len
 client = commands.Bot(command_prefix="#")
 coin = ['HEADs','TAILS']
 dice = ['1','2','3','4','5']
-magic_ball = ['As I see it, yes.','Ask again later','Better not tell you now','Cannot predict now','Concentrate and ask again','Dont count on it','It is certain','It is decidedly so']
+magic_ball = ['As I see it, yes.','Ask again later','Better not tell you now','Cannot predict now','Concentrate and ask again','Dont count on it','It is certain','It is decidedly so',"Hell No!"]
 @client.event
 async def on_ready():
     print(" Bot is ready")
@@ -108,109 +108,39 @@ async def h(ctx):
 @client.command(aliases=['magicball'])
 async def mg(ctx):
     embed = discord.Embed(title='Magic Ball',description='you will get 10 seconds to Ask in your Mind',color=discord.Color.dark_purple())
-    embed.set_thumbnail(url='https://ibb.co/zXh4K2L')
+    embed.set_thumbnail(url='https://image.freepik.com/free-vector/magic-sphere-illustration_1284-6361.jpg')
     embed.add_field(name='Get Advised',value='Ask Any question in your mind and Bot will answer and boost Your confidence')
     await ctx.send(embed=embed)
     await asyncio.sleep(10)
     magic_answer = random.choice(magic_ball)
     embed = discord.Embed(title='Your Answer is ',description=magic_answer,color=discord.Color.dark_purple())
-    embed.set_thumbnail(url='https://ibb.co/zXh4K2L')
+    embed.set_thumbnail(url='https://image.freepik.com/free-vector/magic-sphere-illustration_1284-6361.jpg')
     await ctx.send(embed=embed)
 
 
 
 
-class Userinfo:
+@client.command(aliases=['usif'])
+async def userinfo(ctx, member: discord.Member = None):
+    member = ctx.author if not member else member
+    roles = [role for role in member.roles]
 
-    def __init__(self, bot):
-        self.bot = bot
+    embed = discord.Embed(title='**__Userinfo__**',color=member.color,timestamps=ctx.message.created_at)
+    embed.set_author(name=f"User Info {member}")
+    embed.set_thumbnail(url=member.avatar_url)
+    embed.set_footer(text=f"Requested By {ctx.author}",icon_url=ctx.author.avatar_url)
+    embed.add_field(name='ID',value=member.id)
+    embed.add_field(name='User Name in the server:',value=member.display_name)
+    embed.add_field(name='Created at:',value=member.created_at.strftime("%a, %#d %B %Y, %I:%M %p CST"))
+    embed.add_field(name='Joined at:',value=member.joined_at.strftime("%a, %#d %B %Y, %I:%M %p CST"))
+    embed.add_field(name=f'Roles ({len(roles)})',value=" ".join([role.mention for role in roles]))
+    embed.add_field(name='Most Prior Role:',value=member.top_role.metion)
+    embed.add_field(name='Is Member a Bot?',value=member.bot)
 
-    @commands.group(invoke_without_command=True, aliases=['user', 'uinfo', 'info', 'ui'])
-    async def userinfo(self, ctx, *, name=""):
-        """Get user info. Ex: [p]info @user"""
-        if ctx.invoked_subcommand is None:
-            pre = cmd_prefix_len()
-            if name:
-                try:
-                    user = ctx.message.mentions[0]
-                except IndexError:
-                    user = ctx.guild.get_member_named(name)
-                if not user:
-                    user = ctx.guild.get_member(int(name))
-                if not user:
-                    user = self.bot.get_user(int(name))
-                if not user:
-                    await ctx.send(self.bot.bot_prefix + 'Could not find user.')
-                    return
-            else:
-                user = ctx.message.author
-
-            if user.avatar_url_as(static_format='png')[54:].startswith('a_'):
-                avi = user.avatar_url.rsplit("?", 1)[0]
-            else:
-                avi = user.avatar_url_as(static_format='png')
-            if isinstance(user, discord.Member):
-                role = user.top_role.name
-                if role == "@everyone":
-                    role = "N/A"
-                voice_state = None if not user.voice else user.voice.channel
-            if embed_perms(ctx.message):
-                em = discord.Embed(timestamp=ctx.message.created_at, colour=0x708DD0)
-                em.add_field(name='User ID', value=user.id, inline=True)
-                if isinstance(user, discord.Member):
-                    em.add_field(name='Nick', value=user.nick, inline=True)
-                    em.add_field(name='Status', value=user.status, inline=True)
-                    em.add_field(name='In Voice', value=voice_state, inline=True)
-                    em.add_field(name='Game', value=user.activity, inline=True)
-                    em.add_field(name='Highest Role', value=role, inline=True)
-                em.add_field(name='Account Created', value=user.created_at.__format__('%A, %d. %B %Y @ %H:%M:%S'))
-                if isinstance(user, discord.Member):
-                    em.add_field(name='Join Date', value=user.joined_at.__format__('%A, %d. %B %Y @ %H:%M:%S'))
-                em.set_thumbnail(url=avi)
-                em.set_author(name=user, icon_url='https://i.imgur.com/RHagTDg.png')
-                await ctx.send(embed=em)
-            else:
-                if isinstance(user, discord.Member):
-                    msg = '**User Info:** ```User ID: %s\nNick: %s\nStatus: %s\nIn Voice: %s\nGame: %s\nHighest Role: %s\nAccount Created: %s\nJoin Date: %s\nAvatar url:%s```' % (user.id, user.nick, user.status, voice_state, user.activity, role, user.created_at.__format__('%A, %d. %B %Y @ %H:%M:%S'), user.joined_at.__format__('%A, %d. %B %Y @ %H:%M:%S'), avi)
-                else:
-                    msg = '**User Info:** ```User ID: %s\nAccount Created: %s\nAvatar url:%s```' % (user.id, user.created_at.__format__('%A, %d. %B %Y @ %H:%M:%S'), avi)
-                await ctx.send(self.bot.bot_prefix + msg)
-
-            await ctx.message.delete()
-
-    @userinfo.command()
-    async def avi(self, ctx, txt: str = None):
-        """View bigger version of user's avatar. Ex: [p]info avi @user"""
-        if txt:
-            try:
-                user = ctx.message.mentions[0]
-            except IndexError:
-                user = ctx.guild.get_member_named(txt)
-            if not user:
-                user = ctx.guild.get_member(int(txt))
-            if not user:
-                user = self.bot.get_user(int(txt))
-            if not user:
-                await ctx.send(self.bot.bot_prefix + 'Could not find user.')
-                return
-        else:
-            user = ctx.message.author
-
-        if user.avatar_url_as(static_format='png')[54:].startswith('a_'):
-            avi = user.avatar_url.rsplit("?", 1)[0]
-        else:
-            avi = user.avatar_url_as(static_format='png')
-        if embed_perms(ctx.message):
-            em = discord.Embed(colour=0x708DD0)
-            em.set_image(url=avi)
-            await ctx.send(embed=em)
-        else:
-            await ctx.send(self.bot.bot_prefix + avi)
-        await ctx.message.delete()
+    await ctx.send(embed=embed)
 
 
-def setup(bot):
-    bot.add_cog(Userinfo(bot))
+
 
 
 
